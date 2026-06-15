451e2f0ebf8b2ba08899afe393246dbd05523627
Rene Samson Ambrose <rene.samson21@gmail.com>
2026-06-15 23:18:14 +1000

    Lesson 7 half way through

---
+++ src/Lesson7/Tuple.h
@@ -0,0 +1,60 @@
+/**
+ * TUPLE implementation
+ */
+
+#pragma once
+
+#include <memory>
+#include <type_traits>
+#include <functional>
+
+#include "Metaprogramming.h"
+
+namespace bits_of_q
+{
+    template<typename... ELEMS>
+    struct Tuple
+    {
+        constexpr Tuple() = default;
+    };
+
+    template<typename ELEM0, typename... ELEMS1toN>
+    struct Tuple<ELEM0, ELEMS1toN...> : Tuple<ELEMS1toN...>
+    {
+        explicit constexpr Tuple(ELEM0 e1, ELEMS1toN... rest) :
+        Tuple<ELEMS1toN...>(rest ...), data(e1)
+        {}
+        ELEM0 data;
+    };
+
+    // Following is a deduction guide
+    template<typename T, typename ...Ts>
+    Tuple(T e1, Ts... rest) -> Tuple<std::unwrap_ref_decay_t<T> , std::unwrap_ref_decay_t<Ts>...>;
+
+    template<typename... ELEMS>
+    auto make_tuple(ELEMS... elems)
+    {
+        return Tuple<std::unwrap_ref_decay_t<ELEMS>...>(elems...);
+    }
+
+    ///////////////////////////// Get ////////////////////////////////////////////
+    namespace detail{
+        template<size_t i, typename TUPLE>
+        struct get_impl: get_impl<i-1, pop_front_t<TUPLE>>
+        {};
+        template<typename TUPLE>
+        struct get_impl<0, TUPLE>
+        {
+            template<typename T>
+            constexpr static decltype(auto) get(T& t)
+            {
+                return static_cast<TUPLE&>(t).data;
+            }
+        };
+    } // namespace detail
+    template<size_t i, typename TUPLE>
+    constexpr decltype(auto) get(TUPLE &tuple)
+    {
+        return detail::get_impl<i, std::remove_ref_t<TUPLE>>::get(tuple);
+    }
+} // namespace_bits_of_q
---
+++ src/Lesson7/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * Main function
+ */
+
+#include "Metaprogramming.h"
+#include "Tuple.h"
+
+namespace bits_of_q {} // namespace bits_of_q
+
+int main()
+{
+    using namespace bits_of_q;
+
+    [[maybe_unused]]auto tup = make_tuple(8, true, 1.5);
+    std::cout << tup.data<< "\n";
+
+    Tuple tup2{1, false, 4};
+    std::cout << tup2.data << std::endl;
+}
